d776c4c2593aac66447f5dc1372dbc3804ba159e
Daffakusumaa <122962373+Daffakusumaa@users.noreply.github.com>
2025-09-23 18:51:16 +0700

    Weapon Switching && Weapon Sway

---
+++ Assets/Daffa/Scripts/Weapon/WeaponSway.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WeaponSway : MonoBehaviour
+{
+    [Header("Sway Settings")]
+    [SerializeField] private float smooth;
+    [SerializeField] private float swayMultiplier;
+
+    private void Update()
+    {
+        // Get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * swayMultiplier;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * swayMultiplier;
+
+        // Calculate user rotation
+        Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
+        Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
+
+        Quaternion targetRotation = rotationX * rotationY;
+
+        // Rotate
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+    }
+}
